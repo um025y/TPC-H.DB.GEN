47ca7e0d266af0d44bd118570ce9ad379d07a4fc
um025y <43964221+um025y@users.noreply.github.com>
2019-08-21 13:53:54 +0100

    Add files via upload

--- main_SQL_QUERY_GEN_Ver3_2.py
+++ main_SQL_QUERY_GEN_Ver3_2.py
@@ -1,22 +1,22 @@
 import argparse
 import logging
 import sys
-import Random_Gen_Zipf_Test_2
+import SQL_QUERY_GEN_Ver3_2
 
 logging.basicConfig(level=logging.WARNING, stream = sys.stderr)
 
 # Number of times to execute each query with different query parameter
-maxQueryCnt = 2
+maxQueryCnt = 10
 # Number of iterations to execute query with same query parameter
 maxIterations = 5
-# Total number of different types of quieries
+# Total number of different types of queries
 queryTypeCnt = 8
 
 # Parse Arguments
 parser=argparse.ArgumentParser(description='SOME Description')
 parser.add_argument('--queryCount',     '-Q', help = 'Total number of queries to execute', default = maxQueryCnt, type = int)
 parser.add_argument('--maxIterations',  '-I', help = 'Number of iterations for execution of each query', default = maxIterations, type = int)
-parser.add_argument('--queryTypeCount', '-T', help = 'Total number of different types of quieries', default = queryTypeCnt, type = int)
+parser.add_argument('--queryTypeCount', '-T', help = 'Total number of different types of queries', default = queryTypeCnt, type = int)
 parser.add_argument('--verbose',        '-v', help = 'Turn logging on; specify multiple times for more verbosity', action = 'count')
 
 args = parser.parse_args()
@@ -30,11 +30,11 @@ maxQueryCnt = args.queryCount
 maxIterations = args.maxIterations
 queryTypeCnt = args.queryTypeCount
 
-Random_Gen_Zipf_Test_2.init_query_executor(maxQueryCnt,maxIterations,queryTypeCnt)
+SQL_QUERY_GEN_Ver3_2.init_query_executor(maxQueryCnt, maxIterations, queryTypeCnt)
 
-output = Random_Gen_Zipf_Test_2.run_SQL_executor()
+output = SQL_QUERY_GEN_Ver3_2.run_SQL_executor()
 
 
-Random_Gen_Zipf_Test_2.write_results_to_file('D:\\UofG\\Random_Gen_1_4_Results.csv', output)
+SQL_QUERY_GEN_Ver3_2.write_results_to_file('D:\\UofG\\Random_Gen_1_5_Zipf_Results.csv', output)
 
 
