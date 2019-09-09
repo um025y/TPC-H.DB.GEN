c26f721cafd6e3a07cb48b87ebc530631ac6a1af
Nikos Ntarmos <nikos.ntarmos@glasgow.ac.uk>
2019-09-09 11:17:28 +0100

    Also test for different metrics

--- Code/run-all-tests.sh
+++ Code/run-all-tests.sh
@@ -2,14 +2,16 @@
 
 mkdir -p outputs
 
-for type in 'RANGE' 'JOIN'; do
-	for attr in 'o_orderkey' 'o_totalprice' 'l_orderkey' 'l_extendedprice'; do
-		for outvar in 'Avg_Execution_Time' 'Result_Set_Returned'; do
-			echo "Running experiment for predictions of ${outvar} of ${type} queries with range constraint on ${attr}..."
-			/usr/bin/time -o outputs/r.time-${type}-${attr}-${outvar} \
-				python3 regression.py  -t ${type} -i ${attr} -o ${outvar} \
-				2>>outputs/r.err-${type}-${attr}-${outvar} | \
-				tee -a outputs/r.out-${type}-${attr}-${outvar};
+for scoring in 'neg_mean_absolute_error' 'neg_mean_squared_error' 'explained_variance' 'r2'; do
+	for type in 'RANGE' 'JOIN'; do
+		for attr in 'o_orderkey' 'o_totalprice' 'l_orderkey' 'l_extendedprice'; do
+			for outvar in 'Avg_Execution_Time' 'Result_Set_Returned'; do
+				echo "Running experiment for predictions of ${outvar} of ${type} queries with range constraint on ${attr} and ${scoring} cost function..."
+				/usr/bin/time -o outputs/r.time-${type}-${attr}-${outvar}-${scoring} \
+					python3 regression.py  -t ${type} -i ${attr} -o ${outvar} -s ${scoring}\
+					2>>outputs/r.err-${type}-${attr}-${outvar}-${scoring} | \
+					tee -a outputs/r.out-${type}-${attr}-${outvar}-${scoring};
+			done
 		done
 	done
 done
